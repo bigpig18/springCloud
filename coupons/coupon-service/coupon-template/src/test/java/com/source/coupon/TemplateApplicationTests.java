ce356fadb143e9fa09a4a9b5840fb390ea109d08
liyun <2726829719@qq.com>
2019-11-15 18:01:07 +0800

    "构造优惠券模板测试类"

---
+++ coupons/coupon-service/coupon-template/src/test/java/com/source/coupon/TemplateApplicationTests.java
@@ -0,0 +1,23 @@
+package com.source.coupon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 描述: 优惠券模板系统测试
+ *
+ * @author li
+ * @date 2019/11/15
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TemplateApplicationTests {
+
+
+    @Test
+    public void contextLoad(){}
+
+
+}
