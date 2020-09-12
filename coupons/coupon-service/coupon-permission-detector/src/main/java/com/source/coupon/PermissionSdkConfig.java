0b651abf948dca784c66c26aed2b83c31e853517
liyun <liyun@winorg-data.com>
2020-09-12 11:23:24 +0800

    "权限探测器开启feign功能"

---
+++ coupons/coupon-service/coupon-permission-detector/src/main/java/com/source/coupon/PermissionSdkConfig.java
@@ -0,0 +1,15 @@
+package com.source.coupon;
+
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * 描述: 为了引入PermissionSdk添加的配置类
+ *
+ * @author li
+ * @date 2020/9/12
+ */
+@Configuration
+@EnableFeignClients
+public class PermissionSdkConfig {
+}
